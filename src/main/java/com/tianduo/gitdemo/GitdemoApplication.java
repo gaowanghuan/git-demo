9494c45fe138626f072de8ad2dfcb9f5de3b8e54
gaowanghuan <gaowanghuan@gmail.com>
2025-03-22 21:51:09 +0800

    first commit

---
+++ src/main/java/com/tianduo/gitdemo/GitdemoApplication.java
@@ -0,0 +1,16 @@
+package com.tianduo.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitdemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GitdemoApplication.class, args);
+
+        System.out.println("aaaa");
+    }
+
+}
